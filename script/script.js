8a7321dfa7d026c3aab0ad2894571964a9def314
xmandx <amanda.godinho@etec.sp.gov.br>
2024-01-08 16:23:31 -0300

    first commit

---
+++ script/script.js
@@ -0,0 +1,7 @@
+const modal = document.querySelector('.modal-container')
+function openModal() {
+    modal.classList.add('active')
+}
+function closeModal() {
+    modal.classList.remove('active')
+}
